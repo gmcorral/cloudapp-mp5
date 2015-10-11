8d50df27e77816bdcbdec6e2590017f68011e8b4
Guillermo Menendez <gmcorral@gmail.com>
2015-10-11 22:15:50 +0200

    Compile fix

--- src/KMeansMP.java
+++ src/KMeansMP.java
@@ -93,7 +93,7 @@ public final class KMeansMP {
         JavaRDD<Vector> points = lines.map(new ParsePoint());
         JavaRDD<String> titles = lines.map(new ParseTitle());
 
-        KMeansModel model = KMeans.train(points.rdd(), k, iterations, runs, KMeans.RANDOM(), 0);
+        model = KMeans.train(points.rdd(), k, iterations, runs, KMeans.RANDOM(), 0);
 
         JavaPairRDD<Integer, Iterable<String>> clusters =titles.zip(points).mapToPair(new ClusterCars(model)).groupByKey();
         clusters.foreach(new PrintCluster(model));
